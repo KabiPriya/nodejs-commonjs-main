3abe896014a1a0d376060179b27adb408d53632b
KabiPriya <kabipriya.t@gmail.com>
2023-10-22 23:07:13 +0530

    data CREATE

--- app/index.js
+++ app/index.js
@@ -21,7 +21,7 @@ app.get("/", (req, res) => {
 // add data CREATE
 app.post("/", (req, res) => {
     // console.log("==> ", req.body);
-    // console.log("==> ", req.method);
+    console.log("==> ", req.method);
     console.log("==> ", req.headers);
     console.log("==> ", req.path);
 
